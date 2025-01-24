f7846538307a560ff3d65b53f25c1df81ac9c98e
nikhen-s <nikhen.sanjaya2003@gmail.com>
2025-01-24 13:00:03 +0000

    add notifications folder

---
+++ src/components/modules/notifications/index.tsx
@@ -0,0 +1,11 @@
+import { Card, CardContent } from "../../ui/card";
+import { Button } from "../../ui/button";
+import { ArrowRight } from "lucide-react";
+
+const GetNotificationsCard = () => {
+    <Card className="">
+    <CardContent className="">
+      Get notifications when application opens
+    </CardContent>
+  </Card>
+}
