074856aa58270f0bb468e0e6c6e0858b5924908d
yasiridriz <yasiridriz@gmail.com>
2025-01-25 15:48:37 +0000

    Revert "minor bugs, ux, slight update to styles" (#12)
    Revert "minor bugs, ux, slight update to styles (#11)"
    
    This reverts commit 44774d3d62fef75b370d14a3a6fdb900cbbd412d.

--- src/app/(auth)/signin/page.tsx
+++
@@ -1,9 +0,0 @@
-import { LoginForm } from "./login-form";
-
-export default function LoginPage() {
-  return (
-    <div className="flex min-h-[calc(100vh-4rem)] max-w-screen-md w-full flex-col items-center justify-center">
-      <LoginForm />
-    </div>
-  );
-}
--- src/app/dashboard/profile/page.tsx
+++
@@ -1,9 +0,0 @@
-import { UserProfile } from "@clerk/nextjs";
-
-export default function ProfilePage() {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
-      <UserProfile routing="hash" />
-    </div>
-  );
-}
