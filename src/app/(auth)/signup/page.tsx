6759b5b9c8722a6768b9d93e32b6569f16e447f3
yasiridriz <yasiridriz@gmail.com>
2025-02-10 23:34:32 +0000

    fix: disable signups after 10feb and fix runtime error (#53)
    * bug fixes, leave team from application form
    
    * fix: added correct domain for exeter uni
    
    * fix: security patch, hide member info when retrieving team
    
    * close applications after 10 feb
    
    * fix: disable signups after 10feb and fix runtime error

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,7 +1,19 @@
+import { redirect } from "next/navigation";
 import { SignupForm } from "./signup-form";
 
 
-export default function SignupPage() {
+export default async function SignupPage() {
+  const date = new Date();
+
+  if (
+    (date.getUTCFullYear() == 2025 &&
+      (date.getUTCMonth() > 1 ||
+        (date.getUTCMonth() == 1 && date.getUTCDate() > 10))) ||
+    date.getUTCFullYear() > 2025
+  ) {
+    redirect("/applications-closed");
+  }
+
   return (
     <div className="flex min-h-[calc(100vh-4rem)] mt-14 max-w-screen-md w-full flex-col items-center justify-center">
       <SignupForm />
