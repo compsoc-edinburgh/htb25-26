430fecf3ff69d30141294b7ec249a7a4315c9ca4
tomas <tomasroma64@gmail.com>
2025-02-12 13:03:53 +0000

    add rejection UI

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -3,7 +3,7 @@ import { notFound, redirect } from "next/navigation";
 import { currentUser } from "@clerk/nextjs/server";
 import Link from "next/link";
 import OnboardingForm from "./onboarding-form";
-
+import Image from "next/image";
 export default async function ApplyPage() {
   const clerkUser = await currentUser();
   const user = await api.user.get();
@@ -14,10 +14,53 @@ export default async function ApplyPage() {
 
   const application = await api.application.getUserApplication();
 
-
-
-  if (!application || application.status !== "accepted") {
+  if (!application) {
     notFound();
+  }
+
+  if (application.status !== "accepted") {
+    return (
+      <div className="mx-auto flex min-h-screen w-full max-w-screen-md flex-col justify-center px-2 pb-6 pt-20 md:h-screen md:min-h-full md:pt-6">
+        <div className="mx-auto flex max-w-screen-sm flex-col gap-4">
+          <Image
+            src="/HTB-logo.png"
+            alt="HackTheBurgh Logo"
+            width={300}
+            height={100}
+            className="mb-8 object-contain"
+          />
+          <p>Hey,</p>
+
+          <p>
+            Thank you so much for applying to HackTheBurgh! We appreciate the
+            time and effort you put into your application.{" "}
+          </p>
+
+          <p>
+            Unfortunately, due to the overwhelming number of strong applications
+            we received this year and our limited capacity, we regret to inform
+            you that we cannot offer you a place at this year's event.
+          </p>
+
+          <p>
+            Please don't let this discourage you from applying to future
+            hackathons or tech events. We encourage you to continue developing
+            your skills and pursuing your interest in technology.
+          </p>
+
+          <p>
+            We wish you the best in your future endeavours and hope to see your
+            application again next year!
+          </p>
+
+          <p>
+            Best regards,
+            <br />
+            The HackTheBurgh Team
+          </p>
+        </div>
+      </div>
+    );
   }
 
   return (
