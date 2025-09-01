52e17a8fdeb2c0790e2feb783db60e3c7076a1d1
HJK <danyil.butov@gmail.com>
2025-09-01 11:44:14 +0100

    fix: update footer to add responsivness

--- src/app/(landing)/applications-closed/page.tsx
+++ src/app/(landing)/applications-closed/page.tsx
@@ -1,6 +1,6 @@
 export default async function ApplicationsClosed() {
   return (
-    <main className="h-full w-full">
+    <main className="h-screen w-full">
       <div className="relative flex h-full w-full items-center justify-center">
         <div className="flex scale-90 transform flex-col items-center px-4 text-center sm:scale-100">
           <h1 className="font-hexaframe text-3xl font-extrabold sm:text-4xl md:text-5xl">
