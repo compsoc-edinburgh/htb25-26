af3b3e4df0569c54d3d25f7e382ea2354d18f404
chexxrexx <yuna.kumar@gmail.com>
2025-09-14 16:29:56 +0100

    new page for dashboard this SHOULD work pls pls pls

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,73 +1,14 @@
-import { api } from "~/trpc/server";
-import { auth, currentUser } from "@clerk/nextjs/server";
-import RequestAuth from "~/components/layout/request-auth";
 import { redirect } from "next/navigation";
-import { LogOut, UserIcon } from "lucide-react";
-import { Button } from "~/components/ui/button";
-import { SignOutButton } from "@clerk/nextjs";
-import Image from "next/image";
+import { api } from "~/trpc/server";
+// import EditApplicationForm from "./form";
+import DashboardClient from "./dashboardClient";
 
-export default async function ApplicationPage() {
-  const { userId } = await auth();
-  if (!userId) return <RequestAuth mode="signin" />;
+export default function ApplicationPage() {
+  // const application = await api.application.getUserApplication();
+  // const user = await api.user.get();
 
-  const user = await api.user.get();
-  const application = await api.application.getUserApplication();
+  // if (!application || !user) redirect("/apply");
 
-  if (!application) {
-    redirect("/apply");
-  }
-
-  const clerkUser = await currentUser();
-
-  return (
-    <div className="w-full py-20">
-      <div className="relative mx-auto flex h-28 w-full max-w-screen-xl items-center justify-center overflow-hidden border border-zinc-200 bg-white text-center">
-        <h1 className="text-center text-4xl font-bold">Your profile</h1>
-      </div>
-      <div className="mx-auto max-w-screen-xl">
-        <div className="flex flex-col">
-          <div className="relative flex w-full flex-col items-center justify-between gap-3 border border-zinc-200 bg-white px-6 py-4 text-black transition-all duration-300">
-            <div>
-              <h1>Application Status</h1>
-            </div>
-          </div>
-          {user && (
-            <div className="w-full border border-zinc-200 bg-white p-3 py-6 text-black">
-              <div className="flex items-center justify-between gap-3">
-                {clerkUser?.hasImage ? (
-                  <div className="aspect-square max-h-12 overflow-hidden">
-                    <Image
-                      src={clerkUser.imageUrl}
-                      className="block object-cover"
-                      width={100}
-                      height={100}
-                      alt="User Avatar"
-                    />
-                  </div>
-                ) : (
-                  <UserIcon
-                    strokeWidth={0}
-                    className="bg-gradient aspect-square h-12 w-12 bg-gradient-to-b from-muted to-muted fill-white stroke-muted p-2"
-                  />
-                )}
-                <span className="flex flex-1 flex-col">
-                  <span className="text-xl font-bold">
-                    {user.first_name} {user.last_name}
-                  </span>
-                  <span className="text-sm">{user.email}</span>
-                </span>
-
-                <SignOutButton>
-                  <Button variant="outline">
-                    Sign out <LogOut />
-                  </Button>
-                </SignOutButton>
-              </div>
-            </div>
-          )}
-        </div>
-      </div>
-    </div>
-  );
+  return <DashboardClient />;
+    // <DashboardClient application={application} user={user} />;
 }
