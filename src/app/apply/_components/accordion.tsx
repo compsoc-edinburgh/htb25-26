647bd454b0cfeca5dfa5140fb55ca6af4e718f58
Kay <kumartanmay315@gmail.com>
2025-09-15 11:05:51 +0100

    fix: small changes to accordion header layout

--- src/app/apply/_components/accordion.tsx
+++ src/app/apply/_components/accordion.tsx
@@ -30,7 +30,7 @@ export function AccordionSection({
         type="button"
         onClick={() => onToggle(id)}
         className={cn(
-          "flex w-full items-center justify-between p-8 text-left transition-colors hover:bg-zinc-100 md:p-10 lg:p-12",
+          "relative flex w-full items-center justify-between p-8 text-left transition-colors hover:bg-zinc-100 md:p-10 lg:p-12",
           expanded && "bg-zinc-50",
           disabled && "cursor-not-allowed opacity-50",
           invalid && "bg-red-300"
@@ -40,21 +40,30 @@ export function AccordionSection({
         <div className="flex items-center space-x-4">
           <div className="flex flex-col md:gap-5">
             {typeof questionsCount === "number" && (
-              <span className="mb-2 flex items-center gap-2 text-xs text-zinc-600">
+              <div className="absolute top-[15%] mb-2 flex items-center gap-1 text-[0.5rem] text-zinc-600 sm:left-6 sm:top-4 sm:text-[0.6rem]">
+                <div className="h-1.5 w-1.5 bg-black sm:h-2 sm:w-2" />
                 {questionsCount} QUESTIONS
-              </span>
+              </div>
             )}
-            <h2 className="font-whyte text-2xl font-bold text-black md:text-4xl lg:text-5xl">
+            <h2 className="pt-5 font-whyte text-2xl font-bold text-black md:text-4xl lg:text-5xl">
               {title}
             </h2>
           </div>
         </div>
-        <div className="text-zinc-600">
-          {expanded ? (
-            <ChevronUp className="h-5 w-5" />
-          ) : (
-            <ChevronDown className="h-5 w-5" />
-          )}
+        <div className="absolute right-5 top-1/2 h-2 w-4 sm:h-3 -translate-y-1/2 sm:right-9">
+          <svg
+            width="14"
+            height="13"
+            viewBox="0 0 14 13"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className={cn(
+              "h-full w-full",
+              expanded && "rotate-180"
+            )}
+          >
+            <path d="M1 1L12.5 1L6.5 11.5L1 1Z" fill="black" stroke="black" />
+          </svg>
         </div>
       </button>
 
