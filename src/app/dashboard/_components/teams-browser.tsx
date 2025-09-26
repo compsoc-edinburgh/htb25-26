286a8605d8925d3da95dcdfb3396488c51ebd9b7
chexxrexx <yuna.kumar@gmail.com>
2025-09-26 23:33:30 +0100

    fix viewport height for the teams browser

--- src/app/dashboard/_components/teams-browser.tsx
+++ src/app/dashboard/_components/teams-browser.tsx
@@ -2,7 +2,7 @@
 
 export default function TeamsBrowser() {
     return (
-    <div className="flex h-full w-[98.5%] bg-gray-200 p-4">
+    <div className="flex h-[60vh] w-[98.5%] bg-gray-200 p-4">
         <div className="w-full">
           <p className="ml-2 mt-2 font-hexaframe font-bold text-4xl text-black">TEAMS</p>
   
